d14dbba7d6e3a57a9a00b4242505bb5db8aad3ee
Simon Heidrich <Heidrich@aunovis.de>
2024-12-12 06:00:46 +0100

    Writing first testcase.

--- src/metrics.rs
+++ src/metrics.rs
@@ -2,6 +2,7 @@ use std::path::Path;
 
 use crate::error::Error;
 
+#[derive(PartialEq)]
 #[allow(non_snake_case)]
 pub(crate) struct Metrics {
     archived: Option<f32>,
@@ -16,18 +17,33 @@ impl Metrics {
 
 #[cfg(test)]
 mod tests {
+    use std::fs::write;
+
     use tempfile::NamedTempFile;
 
     use super::*;
 
+    static EXAMPLE_METRIC: Metrics = Metrics {
+        archived: Some(0.1),
+        blocksDeleteOnBranches: Some(0.2),
+    };
+
+    static EXAMPLE_METRIC_STR: &str = r#"
+        archived = 0.1
+        blocksDeleteOnBranches = 0.2
+    "#;
+
     #[test]
     fn metric_can_be_read_from_file() {
         let tempfile = NamedTempFile::new().unwrap();
-        todo!()
+        let filepath = tempfile.path();
+        write(path, EXAMPLE_METRIC_STR).unwrap();
+        let metrics = Metrics::from_file(path).expect("Failed to read from file!");
+        assert!(metrics, EXAMPLE_METRIC);
     }
 
     #[test]
-    fn all_probes_are_optional() {
+    fn archived_probe_is_optional() {
         todo!()
     }
 
