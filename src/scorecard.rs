a1ebd74bb3e0768025e2cb088366044c368373b1
Simon Heidrich <Heidrich@aunovis.de>
2024-12-16 07:57:14 +0100

    Including company name in product name.

--- src/scorecard.rs
+++ src/scorecard.rs
@@ -30,7 +30,7 @@ fn data_dir() -> Result<PathBuf, Error> {
     let app_strategy_args = AppStrategyArgs {
         top_level_domain: "de".to_string(),
         author: "aunovis".to_string(),
-        app_name: "secure_sum".to_string(),
+        app_name: "aunovis_secure_sum".to_string(),
     };
     let data_dir = choose_app_strategy(app_strategy_args)
         .map_err(|e| Error::Other(e.to_string()))?
