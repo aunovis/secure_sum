8ae85747d5c6e2bfce0813603976a9ce372ed253
Simon Heidrich <Heidrich@aunovis.de>
2024-12-16 07:01:45 +0100

    Adding scorecard url function.

--- src/scorecard.rs
+++ src/scorecard.rs
@@ -1,8 +1,26 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use etcetera::{choose_app_strategy, AppStrategy, AppStrategyArgs};
 
 use crate::error::Error;
+
+static CURRENT_VERSION: &str = "5.0.0";
+
+#[cfg(target_os = "macos")]
+static OS_STR: &str = "darwin";
+#[cfg(target_os = "linux")]
+static OS_STR: &str = "linux";
+#[cfg(target_os = "windows")]
+static OS_STR: &str = "windows";
+
+#[cfg(target_arch = "x86_64")]
+static ARCH_STR: &str = "amd64";
+#[cfg(target_arch = "arm")]
+static ARCH_STR: &str = "arm64";
+
+fn scorecard_url() -> String {
+    format!("https://github.com/ossf/scorecard/releases/download/v{CURRENT_VERSION}/scorecard_{CURRENT_VERSION}_{OS_STR}_{ARCH_STR}.tar.gz")
+}
 
 fn data_dir() -> Result<PathBuf, Error> {
     let app_strategy_args = AppStrategyArgs {
@@ -17,10 +35,17 @@ fn data_dir() -> Result<PathBuf, Error> {
 }
 
 fn scorecard_path() -> Result<PathBuf, Error> {
-    Ok(data_dir()?.join("scorecard.exe"))
+    let executable_name = if cfg!(target_os = "windows") {
+        "scorecard.exe"
+    } else {
+        "scorecard"
+    };
+    Ok(data_dir()?.join(executable_name))
 }
 
 fn ensure_scorecard_binary() -> Result<PathBuf, Error> {
+    let path = scorecard_path()?;
+    fs::create_dir_all(&path)?;
     todo!()
 }
 
@@ -29,6 +54,10 @@ mod tests {
     use std::fs;
 
     use super::*;
+
+    fn scorecard_url_exists() {
+        todo!()
+    }
 
     fn data_dir_contains_aunovis_string() {
         let path = scorecard_path().unwrap().to_string_lossy().to_lowercase();
