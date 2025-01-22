beba5f7175c9192e20f3a50823837de1910a5b58
Simon Heidrich <Heidrich@aunovis.de>
2025-01-22 09:15:09 +0100

    Test for Cargo.toml parsing.

--- src/ecosystem/rust.rs
+++ src/ecosystem/rust.rs
@@ -21,3 +21,20 @@ impl DepFile for CargoToml {
         todo!()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::*;
+
+    #[test]
+    fn cargo_toml_can_be_parsed() {
+        let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
+        assert!(path.exists());
+        let result = CargoToml::parse(&path);
+        assert!(result.is_ok(), "{}", result.err().unwrap());
+        let cargo_toml = result.unwrap();
+        assert!(cargo_toml.first_level_deps().contains(&"serde".to_string()));
+    }
+}
