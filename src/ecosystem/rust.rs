17b1ac5984f6521310405a4f8a69669dbb95c1f9
Simon Heidrich <Heidrich@aunovis.de>
2025-01-22 09:38:55 +0100

    Implementing Cargo.toml parsing.

--- src/ecosystem/rust.rs
+++ src/ecosystem/rust.rs
@@ -1,14 +1,26 @@
-use std::path::Path;
+use std::{collections::HashMap, fs, path::Path};
+
+use serde::Deserialize;
 
 use crate::error::Error;
 
 use super::{depfile::DepFile, Ecosystem};
 
-pub(super) struct CargoToml {}
+#[derive(Debug, Deserialize)]
+pub(super) struct CargoToml {
+    #[serde(default)]
+    dependencies: HashMap<String, toml::Value>,
+}
 
 impl CargoToml {
     pub(super) fn parse(file: &Path) -> Result<Self, Error> {
-        todo!()
+        let contents = fs::read_to_string(&file)?;
+        Self::parse_str(&contents)
+    }
+
+    fn parse_str(contents: &str) -> Result<Self, Error> {
+        let cargo_toml = toml::from_str(contents)?;
+        Ok(cargo_toml)
     }
 }
 
@@ -18,7 +30,7 @@ impl DepFile for CargoToml {
     }
 
     fn first_level_deps(&self) -> Vec<String> {
-        todo!()
+        self.dependencies.keys().cloned().collect()
     }
 }
 
@@ -29,7 +41,30 @@ mod tests {
     use super::*;
 
     #[test]
-    fn cargo_toml_can_be_parsed() {
+    fn empty_toml_can_be_parsed() {
+        let result = CargoToml::parse_str("");
+        assert!(result.is_ok(), "{}", result.err().unwrap());
+        let cargo_toml = result.unwrap();
+        assert!(cargo_toml.first_level_deps().is_empty());
+    }
+
+    #[test]
+    fn small_toml_can_be_parsed() {
+        let content = r#"
+        [dependencies]
+        serde = "1.0"
+        toml = { version = "0.5", features = ["derive"] }
+    "#;
+        let result = CargoToml::parse_str(&content);
+        assert!(result.is_ok(), "{}", result.err().unwrap());
+        let cargo_toml = result.unwrap();
+        assert_eq!(cargo_toml.first_level_deps().len(), 2);
+        assert!(cargo_toml.first_level_deps().contains(&"serde".to_string()));
+        assert!(cargo_toml.first_level_deps().contains(&"toml".to_string()));
+    }
+
+    #[test]
+    fn secure_sum_cargo_toml_file_can_be_parsed() {
         let path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("Cargo.toml");
         assert!(path.exists());
         let result = CargoToml::parse(&path);
