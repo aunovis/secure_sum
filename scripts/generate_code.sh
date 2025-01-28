45fd20bc34228d14a17cf4d5ec805cbdc4e4ebab
Simon Heidrich <156177296+aunovis-heidrich@users.noreply.github.com>
2025-01-28 08:14:12 +0100

    Fixing scheduled update check (#37)

---
+++ scripts/generate_code.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+cd $(git rev-parse --show-toplevel)
+
+if [ ! -d ./scripts/venv ]; then
+    python -m venv ./scripts/venv
+fi
+
+if [ -d ./scripts/venv/bin ]; then
+    source ./scripts/venv/bin/activate
+else
+    source ./scripts/venv/Scripts/activate
+fi
+
+pip install -r ./scripts/requirements.txt
+python ./scripts/generate_code.py
+
+cargo fmt
