e9f7d8bd69c2918a5422d302aa6cf25d4cb94c60
Simon Heidrich <Heidrich@aunovis.de>
2025-02-03 07:21:17 +0100

    Updating scripts and requirements.

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,6 +4,10 @@ set -e
 
 cd $(git rev-parse --show-toplevel)
 
+echo Updating Python Packages
+chmod +x ./scripts/update_python_requirements.sh
+./scripts/update_python_requirements.sh
+
 echo Generating Code
 chmod +x ./scripts/generate_code.sh
 ./scripts/generate_code.sh
