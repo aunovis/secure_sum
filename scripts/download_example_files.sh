417bbc270eaf75f8275963ad63f25ae05ebcacae
Simon Heidrich <Heidrich@aunovis.de>
2025-02-05 07:10:51 +0100

    Downloading nuget example files.

--- scripts/download_example_files.sh
+++ scripts/download_example_files.sh
@@ -5,4 +5,11 @@ set -e
 # Change to git root directory
 cd "$(git rev-parse --show-toplevel)"
 
+# Node.js
 curl https://raw.githubusercontent.com/TheComamba/UnKenny/refs/heads/main/package.json > ./system_tests/example_package.json
+
+# NuGet project.assets.json
+curl https://gist.githubusercontent.com/nkolev92/becd4854beaadb9ab53c22f5ccb689f9/raw/80fff62a49ec17935baea516e801908c9ba7f067/sample.project.assets.json > ./system_tests/project.assets.json
+
+# NuGet packages.config
+curl https://raw.githubusercontent.com/dotnet/aspnetcore/refs/heads/main/eng/common/sdl/packages.config > ./system_tests/packages.config
