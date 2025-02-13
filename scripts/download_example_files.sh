193c5e4626168760c89e4614374787e52c0da601
Simon Heidrich <Heidrich@aunovis.de>
2025-02-13 07:44:40 +0100

    Updating system tests.

--- scripts/download_example_files.sh
+++ scripts/download_example_files.sh
@@ -8,8 +8,12 @@ cd "$(git rev-parse --show-toplevel)"
 # Node.js
 curl https://raw.githubusercontent.com/TheComamba/UnKenny/refs/heads/main/package.json > ./system_tests/node_js_package.json
 
+# NuGet project.csproj
+curl https://raw.githubusercontent.com/JamesNK/Newtonsoft.Json/refs/heads/master/Src/Newtonsoft.Json/Newtonsoft.Json.csproj > ./system_tests/nuget_project_1.csproj
+curl https://raw.githubusercontent.com/protobuf-net/protobuf-net/refs/heads/main/Build.csproj > ./system_tests/nuget_project_2.csproj
+
 # NuGet packages.config
 curl https://raw.githubusercontent.com/DotNetAnalyzers/StyleCopAnalyzers/refs/heads/master/.nuget/packages.config > ./system_tests/nuget_packages.config
 
-# Rust (Another package's Cargo.toml)
+# Rust (Another crate's Cargo.toml)
 curl https://raw.githubusercontent.com/TheComamba/LoreCore/refs/heads/main/Cargo.toml >  ./system_tests/rust_cargo.toml
--- system_tests/test.sh
+++ system_tests/test.sh
@@ -4,27 +4,34 @@ set -eou pipefail
 
 # Go to git root folder
 cd "$(git rev-parse --show-toplevel)"
+cd system_tests
 
 # Print help
 cargo run --release -- --help
 
 # Load metrics and run probes on a single repository
-cargo run --release -- ./system_tests/example_metrics.toml https://github.com/aunovis/secure_sum
+cargo run --release -- example_metrics.toml https://github.com/aunovis/secure_sum
 # The second time around, the stored results are used
-cargo run --release -- ./system_tests/example_metrics.toml https://github.com/aunovis/secure_sum
+cargo run --release -- example_metrics.toml https://github.com/aunovis/secure_sum
 # Unless the --rerun flag is used
-cargo run --release -- ./system_tests/example_metrics.toml https://github.com/aunovis/secure_sum --rerun
+cargo run --release -- example_metrics.toml https://github.com/aunovis/secure_sum --rerun
 
 # Run on a dependencyfile of the Rust ecosystem
-cargo run --release -- ./system_tests/example_metrics.toml ./Cargo.toml
+cargo run --release -- example_metrics.toml rust_cargo.toml
 # Run on a dependencyfile of the Node.js ecosystem
-cargo run --release -- ./system_tests/example_metrics.toml ./system_tests/node_js_package.json
-# Run on packages.config file of NuGet ecosystem
-cargo run --release -- ./system_tests/example_metrics.toml ./system_tests/nuget_packages.config
+cargo run --release -- example_metrics.toml node_js_package.json
+# Run on a project.csproj file of the NuGet ecosystem
+cargo run --release -- example_metrics.toml nuget_project_1.csproj
+# Run on a (somewhat old-fashioned) packages.config file of the NuGet ecosystem
+cargo run --release -- example_metrics.toml nuget_packages.config
+
 
 # You can specify more than one dependencyfile
-cargo run --release -- ./system_tests/example_metrics.toml ./Cargo.toml ./system_tests/rust_cargo.toml
+cargo run --release -- example_metrics.toml ../Cargo.toml rust_cargo.toml
 # They may even be from different ecosystem, if for some reason that makes sense in your project
-cargo run --release -- ./system_tests/example_metrics.toml ./Cargo.toml ./system_tests/node_js_package.json
+cargo run --release -- example_metrics.toml rust_cargo.toml node_js_package.json
 # Or mix and match a dependencyfile with URLs
-cargo run --release -- ./system_tests/example_metrics.toml ./Cargo.toml https://github.com/aunovis/secure_sum
+cargo run --release -- example_metrics.toml rust_cargo.toml https://github.com/aunovis/secure_sum
+
+# This makes it easier to run on all .csproj files
+cargo run --release -- example_metrics.toml $(find . -iname "*.csproj")
