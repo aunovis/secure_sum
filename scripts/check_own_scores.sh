bf99ab32dea6af8bb18b55f91b178b57f35b8011
Simon Heidrich <Heidrich@aunovis.de>
2025-03-14 06:05:00 +0100

    Removing time-clutter from script. [no ci]

--- scripts/check_own_scores.sh
+++ scripts/check_own_scores.sh
@@ -6,4 +6,4 @@ cd "$(git rev-parse --show-toplevel)"
 
 cargo build --release
 # Checks all first level dependencies plus the repo itself
-time cargo run --release -- ./reasonable_default_metrics.toml ./Cargo.toml https://github.com/aunovis/secure_sum --details
+cargo run --release -- ./reasonable_default_metrics.toml ./Cargo.toml https://github.com/aunovis/secure_sum --details
