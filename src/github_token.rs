3781e958a05195d3a9010ffaefe545164f079964
Simon Heidrich <Heidrich@aunovis.de>
2025-03-14 07:30:46 +0100

    Began implementing Token check.

--- src/github_token.rs
+++ src/github_token.rs
@@ -1,5 +1,7 @@
 use crate::Error;
 
+static GITHUB_TOKEN_NAME: &str = "GITHUB_TOKEN";
+
 pub(crate) fn ensure_valid_github_token() -> Result<(), Error> {
     let token = get_token()?;
     check_token_validity(&token)
@@ -7,11 +9,29 @@ pub(crate) fn ensure_valid_github_token() -> Result<(), Error> {
 
 fn get_token() -> Result<String, Error> {
     dotenvy::dotenv().ok();
-    todo!()
+    std::env::var(GITHUB_TOKEN_NAME).map_err(|e| {
+        let message = format!("Error reading {GITHUB_TOKEN_NAME} from environment: {e}");
+        Error::Other(message)
+    })
 }
 
 fn check_token_validity(token: &str) -> Result<(), Error> {
-    todo!()
+    static ENDPOINT: &str = "https://api.github.com/rate_limit";
+    static AUTH_HEADER: &str = "Authorization";
+    let header_value = format!("token {token}");
+    let response = reqwest::blocking::Client::new()
+        .get(ENDPOINT)
+        .header(AUTH_HEADER, header_value)
+        // .bearer_auth(token)
+        .send()?
+        .error_for_status();
+    match response {
+        Ok(_) => Ok(()),
+        Err(e) => {
+            let message = format!("Your GITHBU_TOKEN is apparently not valid: {e}");
+            Err(Error::Other(message))
+        }
+    }
 }
 
 #[cfg(test)]
@@ -20,7 +40,6 @@ mod tests {
 
     use super::*;
 
-    static GITHUB_TOKEN_NAME: &str = "GITHUB_TOKEN";
     static TOY_TOKEN: &str = "github_please_trust_me_i_am_a_token";
 
     #[test]
@@ -38,7 +57,7 @@ mod tests {
     #[test]
     #[serial]
     /// For this test to work, a .env file containing the GITHUB_TOKEN needs to exists.
-    fn get_token_reads_dotenv() {
+    fn get_token_reads_dotenv_file() {
         unsafe {
             std::env::remove_var(GITHUB_TOKEN_NAME);
         }
@@ -47,6 +66,7 @@ mod tests {
 
     #[test]
     #[serial]
+    /// For this test to work, a .env file containing the GITHUB_TOKEN needs to exists.
     fn valid_token_is_valid() {
         let token = get_token().unwrap();
         let result = check_token_validity(&token);
