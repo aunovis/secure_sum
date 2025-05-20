077608c8b3c5e5c9e507cd9eed5c50001678b955
Simon Heidrich <Heidrich@aunovis.de>
2025-05-20 06:24:43 +0200

    Re-introducing exmple metric to speed up system test.

--- system_tests/test.sh
+++ system_tests/test.sh
@@ -21,19 +21,19 @@ cargo run --release -- https://github.com/aunovis/secure_sum --details
 cargo run --release -- --metric example_metric.toml https://github.com/aunovis/secure_sum
 
 # Run on a dependencyfile of the Rust ecosystem
-cargo run --release -- rust_cargo.toml
+cargo run --release -- --metric example_metric.toml rust_cargo.toml
 # Run on a dependencyfile of the Node.js ecosystem, with verbose output
-cargo run --release -- node_js_package.json --verbose
+cargo run --release -- --metric example_metric.toml node_js_package.json --verbose
 # Run on a project.csproj file of the NuGet ecosystem, with less output
-cargo run --release -- nuget_project_1.csproj --quiet
+cargo run --release -- --metric example_metric.toml nuget_project_1.csproj --quiet
 # Run on a (somewhat old-fashioned) packages.config file of the NuGet ecosystem
-cargo run --release -- nuget_packages.config
+cargo run --release -- --metric example_metric.toml nuget_packages.config
 
 # You can specify more than one dependencyfile
-cargo run --release -- ../Cargo.toml rust_cargo.toml
+cargo run --release -- --metric example_metric.toml ../Cargo.toml rust_cargo.toml
 # They may even be from different ecosystem, if for some reason that makes sense in your project
-cargo run --release -- rust_cargo.toml node_js_package.json
+cargo run --release -- --metric example_metric.toml rust_cargo.toml node_js_package.json
 # Or mix and match a dependencyfile with URLs
-cargo run --release -- rust_cargo.toml https://github.com/aunovis/secure_sum
+cargo run --release -- --metric example_metric.toml rust_cargo.toml https://github.com/aunovis/secure_sum
 # You can use some bash scripting to include all .csproj files
-cargo run --release -- $(find . -iname "*.csproj")
+cargo run --release -- --metric example_metric.toml $(find . -iname "*.csproj")
