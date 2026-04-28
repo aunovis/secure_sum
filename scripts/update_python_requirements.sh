1b62303318c40703f0641257b5d6cc6eb3a15e3b
Simon Heidrich <Heidrich@aunovis.de>
2026-04-28 07:26:47 +0200

    Hacking together an update mechanism for python.

--- scripts/update_python_requirements.sh
+++ scripts/update_python_requirements.sh
@@ -7,10 +7,17 @@ cd scripts
 
 source ./python_cmd.sh
 
-yesterday=$(date -Is -d "yesterday")
+yesterday=$(date -u -d "1 day ago" +"%Y-%m-%dT%H:%M:%S")
 
 $PYTHON -m pip install --upgrade --uploaded-prior-to=$yesterday pip
 
-pip install --upgrade --uploaded-prior-to=$yesterday --requirement ./requirements.txt
+outdated=$(pip list --outdated --format=json)
+if [[ "$outdated" != "[]" ]]; then
+    echo "$outdated" \
+        | $PYTHON -c "import json,sys; print(' '.join(p['name'] for p in json.load(sys.stdin)))" \
+        | xargs pip install --upgrade --uploaded-prior-to=$yesterday
+else
+    echo "Everything is up to date."
+fi
 
 pip freeze > requirements.txt
